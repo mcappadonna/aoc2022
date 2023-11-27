a8d06a1bb5e588397e3b0d7d8bb272ed31d6a395
Matteo Cappadonna <matteo.cappadonna@gmail.com>
2023-11-27 18:15:12 +0100

    2022/12/04 - Solved

--- day04/day4-2.py
+++ day04/day4-2.py
@@ -1,10 +1,10 @@
 #!/opt/homebrew/bin/python3
 
-def checkContains(elf1: range, elf2: range):
-    if elf1.start >= elf2.start and elf1.stop <= elf2.stop:
-        return True
-    elif elf2.start >= elf1.start and elf2.stop <= elf1.stop:
-        return True
+def overlap(elf1: range, elf2: range):
+    for section1 in elf1:
+        for section2 in elf2:
+            if section1 == section2:
+                return True
     return False
 
 
@@ -14,9 +14,9 @@ with open('input', 'r') as f:
     for pair in f.readlines():
         for pairString in pair.replace('\n', '').split(','):
             pairNumbers = pairString.split('-')
-            pairRange = range(int(pairNumbers[0]), int(pairNumbers[1]))
+            pairRange = range(int(pairNumbers[0]), int(pairNumbers[1])+1)
             elves.append(pairRange)
-        if checkContains(elves[0], elves[1]):
+        if overlap(elves[0], elves[1]):
             fullyContains += 1
         elves = []
     print(fullyContains)
